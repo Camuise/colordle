cecf91f9b0bd95a3c080d608c5839091617703e2
Issac Liu <issac@matchatea.dev>
2025-08-29 01:55:44 -0400

    feat: get todays color without a net request

--- globals.gd
+++ globals.gd
@@ -5,6 +5,15 @@ enum ColorTheme {
     DARK
 }
 @export var theme: ColorTheme = ColorTheme.LIGHT
+enum ColordleFormat {
+    RGB,
+    HEX,
+    HSL,
+    HSV
+}
+@export var colordle_format: ColordleFormat = ColordleFormat.HEX
+@export var todays_color: Color = _get_todays_color()
+
 signal theme_changed(new_theme: ColorTheme)
 
 func _ready() -> void:
@@ -15,3 +24,15 @@ func set_theme(new_theme: ColorTheme) -> void:
     if theme != new_theme:
         theme = new_theme
         emit_signal("theme_changed", theme)
+
+func _get_todays_color() -> Color:
+    # step 1: get today's date (excluding time) in UNIX
+    var _today = Time.get_datetime_dict_from_system()
+    var _today_unix = Time.get_unix_time_from_datetime_dict(_today)
+
+    # step 2: use that to seed a random number generator
+    var rng = RandomNumberGenerator.new()
+    rng.seed = _today_unix
+
+    # step 3: generate a random color
+    return Color(rng.randf(), rng.randf(), rng.randf())
