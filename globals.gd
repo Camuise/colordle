c3464473c27d5c4af337d8f3d565a57e35aa8488
Issac Liu <issac@matchatea.dev>
2025-08-29 10:57:25 -0400

    fix: dull colors with RGB, swap to HSV

--- globals.gd
+++ globals.gd
@@ -27,12 +27,20 @@ func set_theme(new_theme: ColorTheme) -> void:
 
 func _get_todays_color() -> Color:
     # step 1: get today's date (excluding time) in UNIX
-    var _today = Time.get_datetime_dict_from_system()
+    var _today: Dictionary = Time.get_datetime_dict_from_system()
+    # step 2: strip hour, min, and sec
+    _today.erase("hour")
+    _today.erase("minute")
+    _today.erase("second")
+
+    # step 3: convert back to unix time
     var _today_unix = Time.get_unix_time_from_datetime_dict(_today)
 
-    # step 2: use that to seed a random number generator
+    # step 4: use that to seed a random number generator
     var rng = RandomNumberGenerator.new()
     rng.seed = _today_unix
 
-    # step 3: generate a random color
-    return Color(rng.randf(), rng.randf(), rng.randf())
+    # step 5: generate a random color
+    var generated_color = Color.from_hsv(rng.randf(), rng.randf(), rng.randf())
+    print_debug("Generated HSV color: %s" % generated_color)
+    return generated_color
