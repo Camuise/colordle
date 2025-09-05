7248c4c77362c4001e2617a70c92c916d1f43d47
Issac Liu <issac@matchatea.dev>
2025-09-05 19:00:09 -0400

    fix: format selector doesnt work

--- format_select.gd
+++ format_select.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
     for format_name in Globals.ColorFormat.keys():
         add_item(format_name)
 
+    select(Globals.colordle_format)
 
-func item_selected(index: int) -> void:
+func _on_item_selected(index: int) -> void:
     Globals.set_color_format(Globals.ColorFormat.values()[index])
     print_debug("Color format changed to: %s" % Globals.ColorFormat.values()[index])
