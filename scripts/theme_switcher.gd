fc082c78281db025f903713d3faf3da17f9f5f0c
Issac Liu <issac@matchatea.dev>
2025-09-21 17:35:21 -0400

    fix: theme switching didnt work

--- scripts/theme_switcher.gd
+++ scripts/theme_switcher.gd
@@ -4,13 +4,13 @@ signal on_theme_changed(new_theme)
 
 func _pressed() -> void:
     if Globals.theme == Globals.ColorTheme.LIGHT:
-        Globals.theme = Globals.ColorTheme.DARK
+        Globals.set_theme(Globals.ColorTheme.DARK)
         # set image to moon icon
-        self.set_button_icon(load("res://images/theme/moon.png"))
+        self.set_button_icon(load("res://assets/images/theme/moon.png"))
     else:
-        Globals.theme = Globals.ColorTheme.LIGHT
+        Globals.set_theme(Globals.ColorTheme.LIGHT)
         # set image to sun icon
-        self.set_button_icon(load("res://images/theme/sun.png"))
+        self.set_button_icon(load("res://assets/images/theme/sun.png"))
 
     # log the current theme
     print_debug("Theme switched to: %s" % Globals.theme)
