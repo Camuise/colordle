5c08ed1cb21f234031cdac790a7a1a2c694a8880
Issac Liu <issac@matchatea.dev>
2025-09-21 18:03:13 -0400

    feat: give up on rotating camera during transitions

--- camera_2d.gd
+++ camera_2d.gd
@@ -24,13 +24,8 @@ func _on_game_state_changed(old_state: Globals.GameState, new_state: Globals.Gam
         Globals.GameState.OPTIONS: Vector2(0, 720)  # directly below
     }
 
-    # animate camera to new position and rotate a bit away while leaving starting pos
+    # Animate camera to new position and rotate a bit away while leaving starting pos
     if self and new_state in camera_positions:
         var target_position = camera_positions[new_state]
-        var away_rotation = deg_to_rad(30.0) if new_state != Globals.GameState.MAIN_MENU else deg_to_rad(-30.0)
         var tween = create_tween()
-        # First, rotate away from starting position
-        tween.tween_property(self, "rotation", away_rotation, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-        # Then, move to target position and rotate back to final rotation
         tween.tween_property(self, "position", target_position, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-        tween.tween_property(self, "rotation", deg_to_rad(0.1) if new_state != Globals.GameState.MAIN_MENU else deg_to_rad(0.0), 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
