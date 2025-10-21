0ce33059bdb0afbbc0531f345363ea0c47f76845
Issac Liu <issac@matchatea.dev>
2025-10-21 00:22:04 -0400

    feat: make the share button do something

--- scripts/results_marathon.gd
+++ scripts/results_marathon.gd
@@ -3,37 +3,40 @@ extends Control
 @onready var share_button = $ShareButton
 @onready var results_display_node = $ResultsDisplay
 
-# Wrapper instance (MarathonResultsDisplay) separate from raw node
-var results_display
+var results_data
 
 
 class MarathonResultsDisplay:
     var data: Globals.InfinidleStats
-    var _title: Label
+    var _title_node: Label
+    var title: String:
+        set(value):
+            _title_node.text = value
+        get:
+            return _title_node.text
     var _bar_graph: Array = []
     var _results_display_node: Control
 
 
     func _init(results_display_node: Control) -> void:
         _results_display_node = results_display_node
-        _title = results_display_node.get_node("Header/Title")
+        _title_node = results_display_node.get_node("Header/Title")
         for i in range(6):
             _bar_graph.append(results_display_node.get_node("BarGraph/Item%d/ProgressBar" % i))
         pass
 
 
     func set_title(new_title: String) -> void:
-        _title.text = new_title
+        _title_node.text = new_title
 
 
-    func set_streak(current_streak: int) -> void:
-        var streak_display: Label = _results_display_node.get_node("Header/StreakCount")
-        streak_display.text = "Streak: %d" % current_streak
-        pass
+    func get_streak() -> int:
+        return data.total_wins
 
 
     func set_data(infinidle_stats: Globals.InfinidleStats) -> void:
         data = infinidle_stats
+        _results_display_node.get_node("Header/StreakCount").text = "Streak: %d" % data.total_wins
         _update_bar_graph()
 
 
@@ -52,14 +55,23 @@ class MarathonResultsDisplay:
 func _ready() -> void:
     Globals.connect("show_results", Callable(self, "_on_show_results"))
     Globals.connect("infinidle_complete", Callable(self, "_on_infinidle_complete"))
-    results_display = MarathonResultsDisplay.new(results_display_node)
+    results_data = MarathonResultsDisplay.new(results_display_node)
 
 
 func _on_show_results(_puzzle_info: Globals.PuzzleInfo, _game_mode: int, _time_taken: float) -> void:
-    results_display.set_title("Colordle ∞ - %s" % Globals.get_todays_date())
+    results_data.set_title("Colordle ∞ - %s" % Globals.get_todays_date())
 
 
 func _on_infinidle_complete(infinidle_stats: Globals.InfinidleStats) -> void:
-    results_display.set_streak(infinidle_stats.total_wins)
-    results_display.set_data(infinidle_stats)
-    results_display._update_bar_graph()
+    results_data.set_data(infinidle_stats)
+    results_data._update_bar_graph()
+
+
+func _on_share_requested() -> void:
+    # 1. Add title + date
+    var share_text: String = "%s, Streak of %d\n\n" % [results_data.title, results_data.get_streak()]
+
+    # 2. Add grades (emojis)
+
+    # 3. Copy to clipboard
+    DisplayServer.clipboard_set(share_text)
