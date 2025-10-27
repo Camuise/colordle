5a146eb02f36aa921596313de6ef9dbed534c360
Issac Liu <issac@matchatea.dev>
2025-10-26 22:42:02 -0400

    fix: remove extra space in error message and add ColorRect node to tutorial

--- scripts/message_box.gd
+++ scripts/message_box.gd
@@ -9,4 +9,4 @@ func set_message(text: String) -> void:
 
 func _ready() -> void:
     message_label.bbcode_enabled = true
-    set_message("[b]If you see this message, something broke.[/b]\n Please report this to @issac on Slack!")
+    set_message("[b]If you see this message, something broke.[/b]\nPlease report this to @issac on Slack!")
