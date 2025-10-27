5ff6dd4407c486069633b904f4eddb6270e3f6d3
Issac Liu <issac@matchatea.dev>
2025-10-26 22:31:20 -0400

    refactor: remove unused variables and clean up set_message function

--- scripts/message_box.gd
+++ scripts/message_box.gd
@@ -1,13 +1,11 @@
 extends PanelContainer
 
 @onready var message_label: RichTextLabel = $Message
-@onready var font_size: int = message_label.get_theme_font_size("normal_font_size")
-@onready var line_separation: int = message_label.get_theme_constant("line_separation")
-var max_width: int = 200  # maximum width of the message box
+
 
 func set_message(text: String) -> void:
-    # Set the message text and adjust the size of the message box accordingly
     message_label.text = text
+
 
 func _ready() -> void:
     message_label.bbcode_enabled = true
